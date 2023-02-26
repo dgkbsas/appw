066f54aadf99f76799779a9b6cd3edb121aca00f
Die M1 <dgk@dgks-MacBook-Pro.local>
2023-02-26 14:16:09 -0300

    todo funcionando, con prueba de graficos 07

--- app/reportes/dxi/vermas/page.jsx
+++ app/reportes/dxi/vermas/page.jsx
@@ -68,7 +68,7 @@ export default function verMasPage() {
             <span className="tituloStep">⏐ {step}</span>
           </p>
         )}
-        <VerMasItem data={dataItemsDxI[itemIndex].verMas} />
+        {dataItemsDxI[itemIndex].verMas ? <VerMasItem data={dataItemsDxI[itemIndex].verMas} /> : null}
       </div>
     </>
   );
